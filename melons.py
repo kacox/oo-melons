2a81f81ff0d96c330447f3f18d448fb4b70f46fc
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-13 11:46:50 -0700

    Part 1 complete

--- melons.py
+++ melons.py
@@ -2,12 +2,6 @@
 
 class AbstractMelonOrder(object):
     """ An abstract base class that other Melon Orders inherit from. """
-    pass
-
-
-
-class DomesticMelonOrder(object):
-    """A melon order within the USA."""
 
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
@@ -15,35 +9,6 @@ class DomesticMelonOrder(object):
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
-
-
-class InternationalMelonOrder(object):
-    """An international (non-US) melon order."""
-
-    def __init__(self, species, qty, country_code):
-        """Initialize melon order attributes."""
-
-        self.species = species
-        self.qty = qty
-        self.country_code = country_code
-        self.shipped = False
-        self.order_type = "international"
-        self.tax = 0.17
 
     def get_total(self):
         """Calculate price, including tax."""
@@ -62,3 +27,20 @@ class InternationalMelonOrder(object):
         """Return the country code."""
 
         return self.country_code
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+    country_code = "USA"
+    order_type = "domestic"
+    tax = 0.08
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+    order_type = "international"
+    tax = 0.17
+
+    def __init__(self, species, qty, country_code)
+        super().__init__(species, qty)
+        self.country_code = country_code
